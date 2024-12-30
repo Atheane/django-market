66874567182bda4b4299f007cfb080e236b9ed0f
Atheane <eroyant@gmail.com>
2024-12-30 16:50:08 +0100

    (feat) admin django, add fields and migrate

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from listings.models import Band, Listing
+
+@admin.register(Band)
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genre', 'year_formed', 'active')  
+    
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'sold')  
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,9 +1,42 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Band(models.Model):
-    name = models.fields.CharField(max_length=100)
-    
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+        ELECTRONIC = 'E'
+        HOUSE = 'H'
+        DEEP_HOUSE = 'DH'
+        MINIMAL_TECH = 'MT'
+        TECHNO = 'T'
+        CLASSIC = 'C'
+        PROGRESSIVE_ELECTRONIC = 'PE'
+        INDIE_ROCK = 'IR'
+    def __str__(self):
+        return f'{self.name}'
+    name = models.fields.CharField(max_length=10)
+    genre = models.fields.CharField(choices=Genre.choices, default=Genre.HIP_HOP, max_length=2, verbose_name="Genre")
+    biography = models.fields.CharField(default='', max_length=1000)
+    year_formed = models.fields.IntegerField(
+        default=2000,
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)    
     
 class Listing(models.Model):
+    class Type(models.TextChoices):
+        RECORDS = 'R'
+        CLOTHING = 'C'
+        POSTERS = 'P'
+        MISCELLANEOUS = 'M'
+    def __str__(self):
+        return f'{self.title}'
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(null=True)
+    type = models.fields.CharField(choices=Type.choices, default=Type.RECORDS, max_length=2, verbose_name="Type")
